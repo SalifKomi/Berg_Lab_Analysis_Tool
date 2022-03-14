802303b88fad7c3b1144fb626dace07e5db59304
Elias <pingvin@berglab.com>
2022-03-14 15:57:29 +0100

    Changed file structure and added adapted versions of Kilosort running scripts to allow for easy batch processing

--- Utilities/Intan/SaveBinary.m
+++
@@ -1,9 +0,0 @@
-function SaveBinary(bin,path,filename)
-    %SAVEBINARY Summary of this function goes here
-    %   Detailed explanation goes here
-    totName = strcat([path filesep filename '.bin']);
-    fid = fopen(totName, 'w'); 
-    fwrite(fid, bin, 'int16');
-    fclose(fid);  
-end
-
